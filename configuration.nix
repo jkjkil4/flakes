77f54ef9ab2c0648ccd35d721b451b0d94b720c8
jkjkil4 <1173374788@qq.com>
2026-04-16 13:36:40 +0800

    fix

--- configuration.nix
+++ configuration.nix
@@ -161,7 +161,7 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "~/flakes";
+    flake = "/home/jkjkil/flakes";
   };
 
   # List packages installed in system profile. To search, run:
