807580c3a4cd5db6eecb4185dbc7922060e1d1de
jkjkil4 <1173374788@qq.com>
2026-04-21 09:57:03 +0800

    CopilotChat

---
+++ nvim/lua/plugins/CopilotChat.lua
@@ -0,0 +1,30 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+  cmd = "CopilotChat",
+  opts = function()
+    local user = vim.env.USER or "User"
+    user = user:sub(1, 1):upper() .. user:sub(2)
+    return {
+      auto_insert_mode = true,
+      headers = {
+        user = "  " .. user .. " ",
+        assistant = "  Copilot ",
+        tool = "󰊳  Tool ",
+      },
+      window = {
+        width = 0.4,
+      },
+    }
+  end,
+  keys = {
+    {
+      "<leader>aa",
+      function()
+        return require("CopilotChat").toggle()
+      end,
+      desc = "Toggle (CopilotChat)",
+      mode = { "n", "x" },
+    },
+  },
+}
