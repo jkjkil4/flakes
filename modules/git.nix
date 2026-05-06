a9aeb2629b8868cc651d000cf19be9cc03366ccd
jkjkil4 <1173374788@qq.com>
2026-05-06 09:19:22 +0800

    extract git.nix

---
+++ modules/git.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    gh
+  ];
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "jkjkil4";
+      user.email = "1173374788@qq.com";
+      init.defaultBranch = "main";
+    };
+  };
+}
