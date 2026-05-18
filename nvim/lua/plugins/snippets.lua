42014729c90a290acae6723cb8458834d02ff995
jkjkil4 <1173374788@qq.com>
2026-05-19 00:27:42 +0800

    refactor snippets

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -1,13 +1,41 @@
+vim.api.nvim_create_autocmd("DirChanged", {
+  callback = function()
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { vim.fn.getcwd() .. "/.vscode/snippets" },
+    })
+    require("luasnip.loaders.from_vscode").load_standalone({
+      path = vim.fn.getcwd() .. "/.code-snippets",
+    })
+  end,
+})
+
+vim.filetype.add({
+  extension = {
+    ["code-snippets"] = "jsonc",
+  },
+})
+
 return {
-  "L3MON4D3/LuaSnip",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").load({
-        paths = {
-          vim.fn.stdpath("config") .. "/vscode-snippets",
-        },
-      })
-    end,
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").load({
+          paths = {
+            vim.fn.stdpath("config") .. "/snippets",
+          },
+        })
+      end,
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      snippets = {
+        preset = "luasnip",
+      },
+    },
   },
 }
