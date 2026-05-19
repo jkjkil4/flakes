b75e765a8cca9b641838fd3880e0f0699e906f7f
jkjkil4 <1173374788@qq.com>
2026-05-19 12:59:43 +0800

    fix snippets

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -21,13 +21,30 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
-        require("luasnip.loaders.from_vscode").load({
+        require("luasnip.loaders.from_vscode").lazy_load() -- load friendly-snippets
+        require("luasnip.loaders.from_vscode").lazy_load({ -- load workspace snippets
           paths = {
             vim.fn.stdpath("config") .. "/snippets",
           },
         })
       end,
     },
+    opts = function()
+      LazyVim.cmp.actions.snippet_forward = function()
+        if require("luasnip").jumpable(1) then
+          vim.schedule(function()
+            require("luasnip").jump(1)
+          end)
+          return true
+        end
+      end
+      LazyVim.cmp.actions.snippet_stop = function()
+        if require("luasnip").expand_or_jumpable() then -- or just jumpable(1) is fine?
+          require("luasnip").unlink_current()
+          return true
+        end
+      end
+    end,
   },
   {
     "saghen/blink.cmp",
